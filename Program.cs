aadd7bee62beca3f60bbc9ad45f6c01058662c35
WalterLC <83130478+WalterLC@users.noreply.github.com>
2021-06-26 20:51:59 -0600

    version 2

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace ejemplo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Prueba!");
+
         }
     }
 }
